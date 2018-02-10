61c9ba24d1d7fb2f7f11815eba34bedea65332b7
aliboy08 <alistair.ponce@gmail.com>
2018-02-10 21:25:02 +0800

    updated ff tabs

--- ff-equal-heights/ff-equal-heights.js
+++ ff-equal-heights/ff-equal-heights.js
@@ -1,4 +1,4 @@
-/*! FF Equal Heights Plugin v.1 */
+/*! FF Equal Heights v.1 */
 (function($){
 	
 	$.fn.ff_equal_heights = function( options ) {
@@ -24,26 +24,24 @@
 		}
 		
 		// Initialize
-		ff_set_equal_heights($this);
+		set_equal_heights($this);
 		
 		if( settings.enable_on_resize ) {
 			
 			// On resize
 			$(window).resize(function() {
-				
+				$this.css("min-height", "initial");
 				// Disable on window width
 				if( settings.disable_on_width ) {
 					if( window.innerWidth <= settings.disable_on_width ) {
-						$this.css("min-height", "initial");
 						return $this;
 					}
 				}
-		
-				ff_set_equal_heights($this);
+				set_equal_heights($this);
 			});
 		}
 		
-		function ff_set_equal_heights(el){
+		function set_equal_heights(el){
 			var temp_height = 0;
 			el.each(function(){
 				if( temp_height < $(this).outerHeight() ) {
--- ff-tabs/ff-tabs.js
+++ ff-tabs/ff-tabs.js
@@ -1,4 +1,4 @@
-/*! FF Custom Tabs Plugin v.1 */
+/*! FF Tabs v.1 */
 (function($){
 	
 	$.fn.ff_tabs = function(options) {
@@ -8,45 +8,143 @@
 		var settings = $.extend({
 			nav: '.tabs-nav',
 			tabs: '.tabs',
-			on_init: '',
-			after_click: '',
+			same_height: false,
+			show_dropdown_nav: { screen: 768 },
+			on_init: null,
+			after_click: null,
 		}, options );
 		
+		var nav = this.find(settings.nav),
+			tabs = this.find(settings.tabs),
+			tabs_children = tabs.children(),
+			nav_children = nav.find('a'),
+			dropdown_nav,
+			dropdown_nav_trigger;
+			
 		if( typeof settings.on_init === 'function' ) {
 			settings.on_init();
 		}
 		
-		var nav = this.find(settings.nav),
-			tabs = this.find(settings.tabs),
-			target, this_nav_item, target_tab;
+		if( settings.same_height || settings.show_dropdown_nav ) {
+	
+			if( settings.same_height ) {
+				set_same_height();
+			}
+			
+			// Enable Dropdown Nav
+			if( settings.show_dropdown_nav ) {
+				if( $(window).width() <= settings.show_dropdown_nav.screen ){
+					display_dropdown_nav('show');
+				} else {
+					display_dropdown_nav('hide');
+				}
+			}
+			
+			$(window).resize(function(){
+				if( settings.same_height ) {
+					set_same_height();
+				}
+				if( settings.show_dropdown_nav ) {
+					if( $(window).width() <= settings.show_dropdown_nav.screen ){
+						display_dropdown_nav('show');
+					} else {
+						display_dropdown_nav('hide');
+					}
+				}
+			})
+		}
+		
+		// Set same height
+		function set_same_height(){
+			var temp_height = 0;
+			tabs_children.css('min-height', 'initial'); // reset height
+			tabs_children.each(function(){
+				// Get tallest
+				if( $(this).outerHeight() > temp_height )
+					temp_height = $(this).outerHeight();
+			});
+			// Set tallest to all
+			tabs_children.css('min-height', temp_height+'px');
+		}
+		
+		// Create dropdown nav
+		function create_dropdown_nav(){
+			dropdown_nav = nav.clone().addClass('dropdown'); // clone nav for dropdown
+			nav.after(dropdown_nav); // append after
+			dropdown_nav.hide();
+			dropdown_nav_trigger = $('<span class="dropdown-trigger">Select</span>');
+			
+			dropdown_nav.prepend(dropdown_nav_trigger);
+			
+			// create dropdown container for the links
+			var dropdown_container = $('<div class="dropdown-container"></div>');
+			dropdown_nav.append(dropdown_container);
+			
+			dropdown_nav_children = dropdown_nav.find('a');
+			
+			// put dropdown a inside container
+			dropdown_container.prepend(dropdown_nav_children);
+			
+			// Remove class on outside click
+			$(window).click(function() {
+				console.log('hide dropdown');
+				dropdown_container.removeClass('show');
+			});
+			
+			dropdown_nav_trigger.click(function(e){
+				e.stopPropagation();
+				dropdown_container.addClass('show');
+			});
+			
+			// On dropdown nav children click, click matching nav item
+			dropdown_nav_children.click(function(){
+				$(nav_children[$(this).index()]).click();
+			})
+			
+		}
+		
+		function display_dropdown_nav(state){
+			if( state === 'show' ) {
+				nav.hide();
+				dropdown_nav.show();
+			} else {
+				nav.show();
+				dropdown_nav.hide();
+			}
+		}
 			
 		// Hide on init
-		tabs.children().hide();
+		tabs_children.hide();
 		
 		// Set first nav item active
-		$(nav.children('a')[0]).addClass('active');
+		$(nav_children[0]).addClass('active');
 		
 		// Show first tab item
-		$(tabs.children()[0]).fadeIn();
+		$(tabs_children[0]).fadeIn();
 		
-		nav.find('a').click(function(e){
+		nav_children.click(function(e){
 			e.preventDefault();
 			
-			this_nav_item = $(this);
+			var this_nav_item = $(this);
 			
 			if( this_nav_item.hasClass('active') ) {
 				return;
 			}
 			
-			target = this_nav_item.attr('href');
+			var target = this_nav_item.attr('href');
 			
 			// Set nav item active
-			nav.children('a').removeClass('active'); // reset
+			nav_children.removeClass('active'); // reset
 			this_nav_item.addClass('active');
 			
-			target_tab = tabs.find(target);
+			if( typeof dropdown_nav_children !== 'undefined' ){
+				console.log('dropdown_nav_trigger', dropdown_nav_trigger);
+				dropdown_nav_trigger.text(this_nav_item.text());
+			}
+			
+			var target_tab = tabs.find(target);
 			// Show target tab
-			tabs.children().hide(); // reset
+			tabs_children.hide(); // reset
 			target_tab.fadeIn();
 			
 			// After click callback
